16ddebb27f475c3c8564432eec56ebc86044070d
Ji Chen <jaykchen@gmail.com>
2023-07-13 16:27:10 -0400

    @29

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ use store_flows::{get, set};
 #[no_mangle]
 pub fn run() {
     schedule_cron_job(
-        String::from("39 * * * *"),
+        String::from("29 * * * *"),
         String::from("cron_job_evoked"),
         callback,
     );
