93f0b480b77f55b8bce7ded9d0ac976a3b61985b
Ji Chen <jaykchen@gmail.com>
2023-07-16 20:42:35 -0400

    set expire for stored data, add comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use airtable_flows::create_record;
-use chrono::{DateTime, Duration, NaiveDate, Utc};
+use chrono::{DateTime, Duration, Utc};
 use dotenv::dotenv;
 use flowsnet_platform_sdk::logger;
 use http_req::{
@@ -12,7 +12,7 @@ use serde::Deserialize;
 use serde_json::Value;
 use std::collections::HashSet;
 use std::env;
-use store_flows::{get, set};
+use store_flows::{get, set, Expire, ExpireKind};
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -21,7 +21,8 @@ pub async fn run() {
         String::from("8 8 * * *"),
         String::from("cron_job_evoked"),
         callback,
-    ).await;
+    )
+    .await;
 }
 
 async fn callback(_body: Vec<u8>) {
@@ -31,8 +32,8 @@ async fn callback(_body: Vec<u8>) {
     let airtable_token_name = env::var("airtable_token_name").unwrap_or("github".to_string());
     let airtable_base_id = env::var("airtable_base_id").unwrap_or("appNEswczILgUsxML".to_string());
     let airtable_table_name = env::var("airtable_table_name").unwrap_or("mention".to_string());
+    let search_key_word = env::var("KEYWORD").unwrap_or("WASMEDGE".to_string());
 
-    let search_key_word = "WASMEDGE";
     let query_params: Value = serde_json::json!({
         "q": search_key_word,
         "sort": "created",
@@ -41,11 +42,13 @@ async fn callback(_body: Vec<u8>) {
 
     let query_string = serde_urlencoded::to_string(&query_params).unwrap();
     let url_str = format!("https://api.github.com/search/issues?{}", query_string);
+    // further filter with following condition:
     // let url_str = format!("https://api.github.com/search/issues?q={search_key_word}+in:title+state:open+repo:second-state/WasmEdge&sort=created&order=desc");
 
     let url = Uri::try_from(url_str.as_str()).unwrap();
     let mut writer = Vec::new();
 
+    // search on GitHub is subject to a lower rate limit when done without a token
     match Request::new(&url)
         .method(Method::GET)
         .header("User-Agent", "flows-network connector")
@@ -66,7 +69,7 @@ async fn callback(_body: Vec<u8>) {
                 Ok(search_result) => {
                     let now = Utc::now();
                     let one_day_earlier = now - Duration::days(1);
-                    let one_day_earlier = one_day_earlier.date_naive(); 
+                    let one_day_earlier = one_day_earlier.date_naive();
 
                     for item in search_result.items {
                         let name = item.user.login;
@@ -74,17 +77,30 @@ async fn callback(_body: Vec<u8>) {
                         let html_url = item.html_url;
 
                         let time = item.created_at;
-                        let datetime: DateTime<Utc> = time.parse().unwrap(); 
-                        let date: NaiveDate = datetime.date_naive(); 
-
+                        let date;
+                        match time.parse::<DateTime<Utc>>() {
+                            Ok(t) => date = t.date_naive(),
+                            Err(_) => {
+                                continue;
+                            }
+                        }
                         if date > one_day_earlier {
+                            // reported issues/pull_requests stored by key `issue_records` as HashSet<String> in store
+                            // shall expire after 30 days of its creation
+                            let expire = Expire {
+                                kind: ExpireKind::Ex,
+                                value: 1000 * 3600 * 24 * 30,
+                            };
+
                             let mut records: HashSet<String> = get("issue_records")
                                 .and_then(|val| serde_json::from_value(val).ok())
                                 .unwrap_or_default();
 
-                            if !records.contains(&html_url) {
+                            if records.contains(&html_url) {
+                                continue;
+                            } else {
                                 records.insert(html_url.clone());
-                                set("issue_records", serde_json::json!(records), None);
+                                set("issue_records", serde_json::json!(records), Some(expire));
                             }
 
                             let data = serde_json::json!({
